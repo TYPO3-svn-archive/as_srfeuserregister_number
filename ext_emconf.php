bb8c38e6a9cecab91ecfa778c5192755c247373a
typotemp <typotemp@735d13b6-9817-0410-8766-e36946ffe9aa>
2008-12-24 00:04:34 +0000

    inital version
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/as_srfeuserregister_number/trunk@15214 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ ext_emconf.php
@@ -0,0 +1,47 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext: "as_srfeuserregister_number"
+#
+# Auto generated 24-12-2008 00:59
+#
+# Manual updates:
+# Only the data in the array - anything else is removed by next write.
+# "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'Number as Username',
+	'description' => 'Extents sr_feuser_register. A configurable number as a fe_user username is generated. It can be used together with normal registration.',
+	'category' => 'fe',
+	'author' => 'Andreas Steinhuber',
+	'author_email' => 'andreas@typotemp.com',
+	'shy' => '',
+	'dependencies' => 'cms',
+	'conflicts' => '',
+	'priority' => '',
+	'module' => '',
+	'state' => 'beta',
+	'internal' => '',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearCacheOnLoad' => 0,
+	'lockType' => '',
+	'author_company' => '',
+	'version' => '1.0.1',
+	'constraints' => array(
+		'depends' => array(
+			'cms' => '',
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => 'a:8:{s:9:"ChangeLog";s:4:"9e9a";s:10:"README.txt";s:4:"ee2d";s:12:"ext_icon.gif";s:4:"63a4";s:17:"ext_localconf.php";s:4:"bc1b";s:28:"ext_typoscript_constants.txt";s:4:"9fea";s:24:"ext_typoscript_setup.txt";s:4:"ec1f";s:14:"doc/manual.sxw";s:4:"c819";s:41:"pi1/class.ux_tx_srfeuserregister_data.php";s:4:"3fc2";}',
+	'suggests' => array(
+	),
+);
+
+?>
